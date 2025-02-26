e2c6cad84db1955de31e86a0a90abe5633b731fc
Xiaoh Li <111653172+Diskutabell@users.noreply.github.com>
2025-02-26 15:48:39 +0100

    added ignoredbylist

--- src/main/java/org/zeroBzeroT/chatCo/ChatPlayer.java
+++ src/main/java/org/zeroBzeroT/chatCo/ChatPlayer.java
@@ -32,44 +32,58 @@ public class ChatPlayer {
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public void saveIgnoreList(final String p) throws IOException {
-        File oldIgnores = new File(Main.dataFolder, "/ignorelists/" + this.player.getName() + ".txt");
-        this.IgnoreList = new File(Main.dataFolder, "/ignorelists/" + this.playerUUID + ".txt");
+    File oldIgnores = new File(Main.dataFolder, "/ignorelists/" + this.player.getName() + ".txt");
+    this.IgnoreList = new File(Main.dataFolder, "/ignorelists/" + this.playerUUID + ".txt");
+    File ignoredByFile = new File(Main.dataFolder, "/ignorelists/" + Bukkit.getPlayer(p).getUniqueId() + "_ignoredByPlayers.txt");
 
-        if (oldIgnores.exists()) {
-            oldIgnores.renameTo(this.IgnoreList);
-        }
+    if (oldIgnores.exists()) {
+        oldIgnores.renameTo(this.IgnoreList);
+    }
 
-        if (!this.IgnoreList.exists()) {
-            this.IgnoreList.getParentFile().mkdir();
+    if (!this.IgnoreList.exists()) {
+        this.IgnoreList.getParentFile().mkdir();
+        final FileWriter fwo = new FileWriter(this.IgnoreList, true);
+        final BufferedWriter bwo = new BufferedWriter(fwo);
+        bwo.close();
+    }
+
+    if (!p.isEmpty()) {
+        if (!this.isIgnored(p)) {
             final FileWriter fwo = new FileWriter(this.IgnoreList, true);
             final BufferedWriter bwo = new BufferedWriter(fwo);
+            bwo.write(p);
+            bwo.newLine();
+            bwo.close();
+        } else {
+            this.ignores.remove(p);
+            this.ignores.remove("");
+            final FileWriter fwo = new FileWriter(this.IgnoreList);
+            final BufferedWriter bwo = new BufferedWriter(fwo);
+
+            for (final String print : this.ignores) {
+                bwo.write(print);
+                bwo.newLine();
+            }
+
             bwo.close();
         }
-
-        if (!p.isEmpty()) {
-            if (!this.isIgnored(p)) {
-                final FileWriter fwo = new FileWriter(this.IgnoreList, true);
-                final BufferedWriter bwo = new BufferedWriter(fwo);
-                bwo.write(p);
-                bwo.newLine();
-                bwo.close();
-            } else {
-                this.ignores.remove(p);
-                this.ignores.remove("");
-                final FileWriter fwo = new FileWriter(this.IgnoreList);
-                final BufferedWriter bwo = new BufferedWriter(fwo);
-
-                for (final String print : this.ignores) {
-                    bwo.write(print);
-                    bwo.newLine();
-                }
-
-                bwo.close();
-            }
-        }
-
-        this.updateIgnoreList();
     }
+
+    // in diesem ganzen code ist kein einziges kommentar lol
+    if (!ignoredByFile.exists()) {
+        ignoredByFile.getParentFile().mkdirs();
+        ignoredByFile.createNewFile();
+    }
+
+    final FileWriter fwoBy = new FileWriter(ignoredByFile, true);
+    final BufferedWriter bwoBy = new BufferedWriter(fwoBy);
+    bwoBy.write(this.player.getName() + " ignores you");
+    bwoBy.newLine();
+    bwoBy.close();
+
+    this.updateIgnoreList();
+}
+
 
     public void unIgnoreAll() throws IOException {
         final FileWriter fwo = new FileWriter(this.IgnoreList, false);
@@ -104,24 +118,47 @@ public class ChatPlayer {
         this.LastReceiver = sender.getName();
     }
 
-    private void updateIgnoreList() throws IOException {
-        final FileInputStream file = new FileInputStream(this.IgnoreList);
-        final InputStreamReader fileReader = new InputStreamReader(file);
-        final BufferedReader inIgnores = new BufferedReader(fileReader);
-        String data = inIgnores.readLine();
-        this.ignores = new ArrayList<>();
+   private void updateIgnoreList() throws IOException {
+    final FileInputStream file = new FileInputStream(this.IgnoreList);
+    final InputStreamReader fileReader = new InputStreamReader(file);
+    final BufferedReader inIgnores = new BufferedReader(fileReader);
+    String data = inIgnores.readLine();
+    this.ignores = new ArrayList<>();
 
-        while (data != null) {
-            this.ignores.add(data);
-            data = inIgnores.readLine();
-        }
-
-        file.close();
+    while (data != null) {
+        this.ignores.add(data);
+        data = inIgnores.readLine();
     }
+    file.close();
+
+    File ignoredByFile = new File(Main.dataFolder, "/ignorelists/" + this.playerUUID + "_ignoredByPlayers.txt");
+
+    if (ignoredByFile.exists()) {
+        final FileInputStream ignoredByStream = new FileInputStream(ignoredByFile);
+        final InputStreamReader ignoredByReader = new InputStreamReader(ignoredByStream);
+        final BufferedReader inIgnoredBy = new BufferedReader(ignoredByReader);
+        String ignoredByData = inIgnoredBy.readLine();
+        this.ignoredBy = new ArrayList<>();
+
+        while (ignoredByData != null) {
+            this.ignoredBy.add(ignoredByData);
+            ignoredByData = inIgnoredBy.readLine();
+        }
+        ignoredByStream.close();
+    } else {
+        this.ignoredBy = new ArrayList<>();
+    }
+}
+
 
     public boolean isIgnored(final String p) {
-        return this.ignores.contains(p);
-    }
+    return this.ignores.contains(p);
+}
+
+public boolean isIgnoredBy(final String p) {
+    return this.ignoredBy.contains(p + " ignores you");
+}
+
 
     public List<String> getIgnoreList() {
         return this.ignores;
